014630cb5d29b1780a9325ac3498101ba8a65990
wirex studio <wirexstudio@outlook.com>
2019-02-28 16:39:49 +0100

    second commit

---
+++ FirsttProject/FirsttProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirsttProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is my first project, test project
+            //It will be on github:)
+
+            Console.Write("This is my first project on Visual Studio :)");
+            Console.WriteLine("by Wirex Studio");
+        }
+    }
+}
