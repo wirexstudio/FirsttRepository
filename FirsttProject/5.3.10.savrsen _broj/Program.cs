8390ad2e83a5951bcbc9dc880963f048495b12e1
wirex studio <wirexstudio@outlook.com>
2019-03-01 23:55:22 +0100

    5.3.10

--- FirsttProject/5.3.10.savrsen _broj/Program.cs
+++ FirsttProject/5.3.10.savrsen _broj/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,26 +12,62 @@ namespace _5._3._10.savrsen__broj
         static void Main(string[] args)
         {
             Console.Write("Unesite prirodan broj: ");
-            int broj = int.Parse(Console.ReadLine());
-            int djelitelji = 0;
 
-            for(int i = 1; i < broj; i++)
+            try
             {
-                if(broj % i == 0)
+                int broj = int.Parse(Console.ReadLine());
+                if(broj <= 0)
                 {
-                    djelitelji += i;
+                    throw new PozitivanBrojException("UNESITE POZITIVAN BROJ!");
+                }
+
+                int djelitelji = 0;
+
+                for (int i = 1; i < broj; i++)
+                {
+                    if (broj % i == 0)
+                    {
+                        djelitelji += i;
+                    }
+                }
+
+                if (djelitelji == broj)
+                {
+                    Console.WriteLine("Broj je savršen.");
+                }
+                else
+                {
+                    Console.WriteLine("Broj nije savršen.");
                 }
             }
-
-            if(djelitelji == broj)
+            catch (FormatException ex)
             {
-                Console.WriteLine("Broj je savršen.");
+                Console.WriteLine("Greška. Poruka: {0}", ex.ToString());
             }
-            else
+            catch(PozitivanBrojException ex)
             {
-                Console.WriteLine("Broj nije savršen.");
+                Console.WriteLine("Greška. Poruka: {0}", ex.ToString());
             }
-            Console.ReadLine();
+            finally
+            {
+                Console.ReadLine();
+            }
         }
+    }
+
+    public class PozitivanBrojException : Exception
+    {
+        public PozitivanBrojException()
+        {
+        }
+
+        public PozitivanBrojException(string message) : base(message)
+        {
+        }
+
+        public PozitivanBrojException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
     }
 }
